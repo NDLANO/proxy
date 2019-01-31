984dd45c434d6575bc75d8360b35d5a95671f1a9
Henrik Nårstad <henrik.narstad@knowit.no>
2019-01-31 13:22:36 +0100

    adapt to kubernetes

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ COPY nginx.conf /etc/nginx/nginx.conf
 COPY nginx-caches-default.conf /etc/nginx/
 COPY nginx-caches-prod.conf /etc/nginx/
 COPY nginx-cacheless.conf /etc/nginx/
+COPY old-hostnames.conf /etc/nginx/
+COPY new-hostnames.conf /etc/nginx/
 
 COPY start-nginx.sh /start-nginx.sh
 
--- start-nginx.sh
+++ start-nginx.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+function is_kubernetes {
+    [ -e "/var/run/secrets/kubernetes.io" ]
+}
+
+function setup_hostnames {
+    if [ is_kubernetes ]; then
+	    ln -fs /etc/nginx/new-hostnames.conf /etc/nginx/hostnames.conf
+    else
+	    ln -fs /etc/nginx/old-hostnames.conf /etc/nginx/hostnames.conf
+    fi
+}
+
 function replace_env_variables {
     sed -i -- "s/NDLA_ENVIRONMENT/$NDLA_ENVIRONMENT/g" /etc/nginx/nginx.conf
+    sed -i -- "s/NDLA_ENVIRONMENT/$NDLA_ENVIRONMENT/g" /etc/nginx/hostnames.conf
 }
 
 function setup_nginx_caches {
@@ -12,8 +25,18 @@ function setup_nginx_caches {
     fi
 }
 
-replace_env_variables
+function setup_dns_resolver {
+    if is_kubernetes; then # Check whether we are running on kubernetes or not
+        echo "resolver kube-dns.kube-system.svc.cluster.local;" > /etc/nginx/nginx-resolver.conf
+    else
+        echo "resolver 127.0.0.11;" > /etc/nginx/nginx-resolver.conf
+    fi
+}
 
+
+setup_hostnames
+replace_env_variables
 setup_nginx_caches
+setup_dns_resolver
 
 nginx -g 'daemon off;'
